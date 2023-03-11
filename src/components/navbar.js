03cdbea150939206165b6b3d08ad4c48580fd7e0
gixion1010 <gixion1337@gmail.com>
2023-03-11 13:12:23 +0500

    navbar completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,30 @@
+import './navbar.css';
 import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import { MenuItems } from './MenuItems';
+
 function Navbar() {
-    return (
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);  
+  return (
     <>
         <nav className='Navbar'>
            <div className="navbar-container">
-            <h1>yo gohar</h1>
+            <h1 className='nav-logo'>GU tech</h1> 
+            <div className="menu-icons" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              {MenuItems.map((item, index) => {
+                return(
+                  <li key = {index}> 
+                   <Link className={item.cName }>
+                     <i class={item.icon}></i> {item.title}
+                   </Link>
+                  </li>
+                )
+              })}
+            </ul>
            </div>
         </nav>
     </>
